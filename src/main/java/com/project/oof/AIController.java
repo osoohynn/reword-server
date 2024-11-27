fe3ee61f28b54f7ede8d866eab3ba621ecd33967
su080915 <tbr335@naver.com>
2024-11-27 23:45:13 +0900

    gpt 연결은 함

--- src/main/java/com/project/oof/AIController.java
+++ src/main/java/com/project/oof/AIController.java
@@ -6,29 +6,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RequestMapping("/api")
 @RestController
 public class AIController {
     private final OpenAiChatModel openAiChatModel;
-//    private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
 
     public AIController(OpenAiChatModel openAiChatModel) {
         this.openAiChatModel = openAiChatModel;
-//        this.vertexAiGeminiChatModel = vertexAiGeminiChatModel;
     }
 
     @GetMapping("/chat")
     public String chat(@RequestBody String message) {
-//        Map<String, String> responses = new HashMap<>();
 
         return openAiChatModel.call(message);
-//        responses.put("openai(chatGPT) 응답", openAiResponse);
-
-//        String vertexAiGeminiResponse = vertexAiGeminiChatModel.call(message);
-//        responses.put("vertexai(gemini) 응답", vertexAiGeminiResponse);
-//        return responses;
     }
 }
